b20bb3a9e778abf60a93d954f595ffb0e3d650e5
jevin <jevin@quickjack.ca>
2022-03-13 15:53:06 -0400

    Successfully disabling on MQTT message

--- README.md
+++ README.md
@@ -18,3 +18,7 @@ To configure it, you can review the configuration options here: https://github.c
 Change the array of services in: `blocked_services`. Full list is in the [Adguard Repo in SERVICES](https://github.com/AdguardTeam/AdGuardHome/blob/master/client/src/helpers/constants.js)
 OR
 just add the rule for blocking that you want. For example, to block all of Slack (so many distracting notifications!!) and it's subdomains: `||slack.com^` as new line in the `user_rules`. More details on how to write the rules is on the [Adguard page](https://github.com/AdguardTeam/AdGuardHome/wiki/Hosts-Blocklists#adblock-style)
+
+# How it works
+
+To recreate to blocking you can run using `httpie`: `http -a admin:password POST http://localhost:3001/control/dns_config <<<'{"protection_enabled": true}'`
--- middleware/main.py
+++ middleware/main.py
@@ -1,8 +1,11 @@
 import paho.mqtt.client as mqtt
+import requests
+from requests.auth import HTTPBasicAuth
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
+    blocking_disabled()
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
@@ -12,8 +15,20 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, message):
     # print(msg.topic+" "+str(msg.payload))
     if message.topic == 'focus-time':
-        print("Focus time requested")
+        print("Focus time mqtt message detected")
+        blocking_enabled()
         
+def blocking_enabled():
+    requests.post( 'http://adguard/control/dns_config', 
+      json={'protection_enabled': True},
+      auth=HTTPBasicAuth('admin', 'password')
+    )
+
+def blocking_disabled():
+    requests.post( 'http://adguard/control/dns_config', 
+      json={'protection_enabled': False},
+      auth=HTTPBasicAuth('admin', 'password')
+    )
 
 client = mqtt.Client("adguard-middleware")
 client.on_connect = on_connect
